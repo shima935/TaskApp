f1d1ae441ebb1fff826cd4db048cbc054511ca6d
shima <shimax.kuma@gmail.com>
2017-11-17 20:10:32 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kumiko/shima/taskapp/MainActivity.java
@@ -0,0 +1,159 @@
+package jp.techacademy.kumiko.shima.taskapp;
+
+
+
+
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.Date;
+import java.util.List;
+import android.support.design.widget.Snackbar;
+import java.util.ArrayList;
+
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_TASK = "jp.techacademy.kumiko.shima.taskapp.TASK";
+
+    private Realm mRealm;
+    private RealmChangeListener mRealmListener = new RealmChangeListener() {
+        @Override
+        public void onChange(Object element) {
+            reloadListView();
+        }
+    };
+
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        mRealm = Realm.getDefaultInstance();
+        mRealm.addChangeListener(mRealmListener);
+
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //入力編集画面に遷移
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task.getId());
+
+                startActivity(intent);
+            }
+        });
+        //ListViewを長押ししたときの処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //タスクを削除する
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                //ダイアログを表示
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.deleteAllFromRealm();
+                        mRealm.commitTransaction();
+
+                        reloadListView();
+
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            }
+        });
+
+        reloadListView();
+    }
+
+    private void reloadListView() {
+        RealmResults<Task> taskRealmResults = mRealm.where(Task.class).findAllSorted("date", Sort.DESCENDING);
+        mTaskAdapter.setTaskList(mRealm.copyFromRealm(taskRealmResults));
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mRealm.close();
+    }
+
+}
+    /*rivate void addTaskForTest(){
+        Task task = new Task();
+        task.setTitle("作業");
+        task.setContents("プログラムを書いてPUSHする");
+        task.setDate(new Date());
+        task.setId(0);
+        mRealm.beginTransaction();
+        mRealm.copyToRealmOrUpdate(task);
+        mRealm.commitTransaction();
+    }
+
+}
+
+
+
+/*
+        //後でTaskクラスに変更//ここから
+        List<String> taskList = new ArrayList<String>();
+
+        taskList.add("aaa");
+        taskList.add("bbb");
+        taskList.add("ccc");
+
+        mTaskAdapter.setTaskList(taskList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
+
+    }
+
+}
+*/
